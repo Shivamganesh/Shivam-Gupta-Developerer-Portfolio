2cecd0c5a1264cd51967399d5de718dcb7d1e601
Shivam Gupta <118615006+Shivamganesh@users.noreply.github.com>
2025-07-12 22:55:37 +0530

    Update projectShowcaseData.js

--- src/data/projectShowcaseData.js
+++ src/data/projectShowcaseData.js
@@ -5,28 +5,28 @@ const projectShowcaseData = [
     id: "Grocygo",
     title: "GrocyGo",
     description: "Grocery Delivery UI",
-    imageUrl: "public/Projectimage/GrocyGo4.png",
+    imageUrl: "/Projectimage/GrocyGo4.png",
     bgColor: "#f4f0e5", // cream background
   },
   {
     id: "aty",
     title: "ShareIt",
     description: "File Sharing UI",
-    imageUrl: "public/Projectimage/shrareit3.png",
+    imageUrl: "/Projectimage/shrareit3.png",
     bgColor: "#e8f4f0", // light mint background
   },
   {
     id: "mic",
     title: "RideOn",
     description: "Cab Booking UI",
-    imageUrl: "public/Projectimage/ride2.png",
+    imageUrl: "/Projectimage/ride2.png",
     bgColor: "#f2e8f4", // light lavender background
   },
   {
     id: "tapso",
     title: "Ekart",
     description: "Shopping App UI",
-    imageUrl: "public/Projectimage/ekart2..png",
+    imageUrl: "/Projectimage/ekart2..png",
     bgColor: "#f1f4e8", // light sage background
   },
 ];
